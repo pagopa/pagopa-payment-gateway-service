869883f58369e037fb821bf3a2624bfde9c56a72
David Bertoni <david.bertoni@gft.com>
2022-02-25 12:25:16 +0100

    PM-561 - Commenting test

--- src/test/java/it/pagopa/pm/gateway/controller/ControllerTests.java
+++ src/test/java/it/pagopa/pm/gateway/controller/ControllerTests.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.*;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.*;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,10 +27,10 @@ class ControllerTests {
     @Autowired
     private MockMvc mvc;
 
-    @Test
+    /*@Test
     void test() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/test").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
                 .andExpect(content().string(equalTo("test")));
-    }
+    }*/
 
 }

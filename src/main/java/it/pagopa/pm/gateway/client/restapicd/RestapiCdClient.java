86ed91dd564bb7a7bc9e88404a112891a5ac7e54
a99z <ignazio.maniscalco@gft.com>
2022-05-27 15:54:25 +0200

    PM-740 - Little Fixes, Added Error Field into Entity

--- src/main/java/it/pagopa/pm/gateway/client/restapicd/RestapiCdClient.java
+++ src/main/java/it/pagopa/pm/gateway/client/restapicd/RestapiCdClient.java
@@ -11,7 +11,7 @@ public interface RestapiCdClient {
     @RequestLine("PATCH /pp-restapi-CD/v1/transactions/update-status/{id}")
     void updateTransaction(@Param Long id, @HeaderMap Map<String, Object> headerMap, TransactionUpdateRequestData transactionUpdateRequest);
 
-    @RequestLine("POST /pp-restapi-CD/v1/payments/close-payment/{id}")
-    String closePayment(@Param Long idTransaction, @HeaderMap Map<String, Object> headerMap, boolean outcome);
+    @RequestLine("POST /pp-restapi-CD/v1/payments/close-payment/{idTransaction}?outcome={outcome}")
+    String closePayment(@Param Long idTransaction, @Param boolean outcome, @HeaderMap Map<String, Object> headerMap);
 
 }

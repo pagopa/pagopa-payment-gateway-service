9efdf01fb183c9f6967950e29dc7e8057c343a15
David Bertoni <david.bertoni@gft.com>
2022-06-20 15:11:59 +0200

    fix

--- devops/db/liquibase/run_image.sh
+++ devops/db/liquibase/run_image.sh
@@ -1,7 +1,7 @@
-/home/oracle/liquibase/liquibase --changeLogFile=/home/oracle/changelogs/payment-manager/master-changelog.xml --url="jdbc:oracle:thin:@localhost:1521:ORCLCDB" --username=PPTEST --password=BW7P8U --log-level=INFO --driver=oracle.jdbc.driver.OracleDriver --classpath=/home/oracle/liquibase/com.oracle.ojdbc8-12.2.0.1.jar update
+/home/oracle/liquibase/liquibase --changeLogFile=/home/oracle/changelogs/payment-transaction-gateway/master-changelog.xml --url="jdbc:oracle:thin:@host.docker.internal:1521:ORCLCDB" --username=PPTEST --password=BW7P8U --log-level=INFO --driver=oracle.jdbc.driver.OracleDriver --classpath=/home/oracle/liquibase/com.oracle.ojdbc8-12.2.0.1.jar update
 
 while [ $? -ne 0 ]
 do
 	sleep 60s
-	/home/oracle/liquibase/liquibase --changeLogFile=/home/oracle/changelogs/payment-manager/master-changelog.xml --url="jdbc:oracle:thin:@localhost:1521:ORCLCDB" --username=PPTEST --password=BW7P8U --log-level=INFO --driver=oracle.jdbc.driver.OracleDriver --classpath=/home/oracle/liquibase/com.oracle.ojdbc8-12.2.0.1.jar update
+	/home/oracle/liquibase/liquibase --changeLogFile=/home/oracle/changelogs/payment-transaction-gateway/master-changelog.xml --url="jdbc:oracle:thin:@host.docker.internal:1521:ORCLCDB" --username=PPTEST --password=BW7P8U --log-level=INFO --driver=oracle.jdbc.driver.OracleDriver --classpath=/home/oracle/liquibase/com.oracle.ojdbc8-12.2.0.1.jar update
 done

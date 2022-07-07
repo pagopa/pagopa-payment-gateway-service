e7e18f407fa6560c983b328aa517c18277d77668
DavideAmatoNexi <Davide.Amato@external.nexigroup.com>
2022-07-07 11:40:26 +0200

    PM-900 - Add StatusErrorCodeOutcomeEnum and changed get flow

--- src/main/java/it/pagopa/pm/gateway/dto/enums/StatusErrorCodeOutcomeEnum.java
+++ src/main/java/it/pagopa/pm/gateway/dto/enums/StatusErrorCodeOutcomeEnum.java
@@ -7,7 +7,8 @@ public enum StatusErrorCodeOutcomeEnum {
 
     GENERIC_ERROR(1L, "Generic error"),
     TIMEOUT( 4L,"Timeout"),
-    MISSING_FIELDS( 6L,"Missing mandatory fields");
+    MISSING_FIELDS( 6L,"Missing fields"),
+    NOT_FOUND( 11L,"404 Not Found");
 
     @Getter
     private final Long outcome;
@@ -19,12 +20,14 @@ public enum StatusErrorCodeOutcomeEnum {
         this.status = status;
     }
 
-    StatusErrorCodeOutcomeEnum getEnum(ExceptionsEnum exceptionsEnum) {
+    public static StatusErrorCodeOutcomeEnum getEnum(ExceptionsEnum exceptionsEnum) {
         switch (exceptionsEnum) {
             case TIMEOUT:
                 return TIMEOUT;
             case MISSING_FIELDS:
                 return MISSING_FIELDS;
+            case TRANSACTION_NOT_FOUND:
+                return NOT_FOUND;
             default:
                 return GENERIC_ERROR;
         }

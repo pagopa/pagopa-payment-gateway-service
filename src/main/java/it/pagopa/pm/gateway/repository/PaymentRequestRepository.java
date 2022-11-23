8948528ad07094d8d39b25e1b572ef854a143cce
urusso <umberto.russo@gft.com>
2022-11-23 17:23:30 +0100

    PM-1217 CreditCard getPaymentInfo implemented

--- src/main/java/it/pagopa/pm/gateway/repository/PaymentRequestRepository.java
+++ src/main/java/it/pagopa/pm/gateway/repository/PaymentRequestRepository.java
@@ -4,6 +4,8 @@ import it.pagopa.pm.gateway.entity.PaymentRequestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PaymentRequestRepository extends JpaRepository<PaymentRequestEntity, Long> {
 
@@ -12,5 +14,7 @@ public interface PaymentRequestRepository extends JpaRepository<PaymentRequestEn
     PaymentRequestEntity findByCorrelationIdAndRequestEndpoint(String correlationId, String requestEndpoint);
 
     PaymentRequestEntity findByGuid(String guid);
+
+    Optional<PaymentRequestEntity> findByGuidAndRequestEndpoint(String guid, String reuqestEndpoint);
 
 }

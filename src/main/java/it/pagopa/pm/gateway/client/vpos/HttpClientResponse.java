323a6bf67e30f3ccf102754de861592c10842d79
Alessandro Dossi <alessandro.dossi@external.nexigroup.com>
2022-11-29 12:11:11 +0100

    PM-1214 - some refactoring

--- src/main/java/it/pagopa/pm/gateway/client/vpos/HttpClientResponse.java
+++ src/main/java/it/pagopa/pm/gateway/client/vpos/HttpClientResponse.java
@@ -1,46 +1,15 @@
 package it.pagopa.pm.gateway.client.vpos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
-/**
- * The Class HttpClientResponse.
- *
- */
+@Getter
+@Setter
 public class HttpClientResponse implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private int status;
     private byte[] entity;
-
-    /**
-     * @return the status
-     */
-    public int getStatus() {
-        return status;
-    }
-
-    /**
-     * @param status
-     *            the status to set
-     */
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    /**
-     * @return the entity
-     */
-    public byte[] getEntity() {
-        return entity;
-    }
-
-    /**
-     * @param entity
-     *            the entity to set
-     */
-    public void setEntity(byte[] entity) {
-        this.entity = entity;
-    }
-
 }
-

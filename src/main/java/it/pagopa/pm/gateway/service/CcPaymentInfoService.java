26fd19db9d7ba6af3c4e65e36b2cccaff96ced42
DavideAmatoNexi <Davide.Amato@external.nexigroup.com>
2023-02-01 10:32:58 +0100

    PM-1320 Changed Get response

--- src/main/java/it/pagopa/pm/gateway/service/CcPaymentInfoService.java
+++ src/main/java/it/pagopa/pm/gateway/service/CcPaymentInfoService.java
@@ -1,15 +1,19 @@
 package it.pagopa.pm.gateway.service;
 
+import com.google.gson.Gson;
 import it.pagopa.pm.gateway.dto.ClientConfig;
 import it.pagopa.pm.gateway.dto.vpos.CcHttpException;
 import it.pagopa.pm.gateway.dto.vpos.CcPaymentInfoResponse;
+import it.pagopa.pm.gateway.dto.vpos.ThreeDsMethodData;
 import it.pagopa.pm.gateway.entity.PaymentRequestEntity;
 import it.pagopa.pm.gateway.repository.PaymentRequestRepository;
 import it.pagopa.pm.gateway.utils.ClientsConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Base64Utils;
 
 import java.util.Optional;
 
@@ -19,6 +23,10 @@ import static it.pagopa.pm.gateway.constant.ApiPaths.REQUEST_PAYMENTS_VPOS;
 @Service
 public class CcPaymentInfoService {
     private static final String AUTHORIZED = "AUTHORIZED";
+    private static final String METHOD = "METHOD";
+
+    @Value("${vpos.method.notifyUrl}")
+    String methodNotifyUrl;
 
     @Autowired
     private PaymentRequestRepository paymentRequestRepository;
@@ -29,7 +37,7 @@ public class CcPaymentInfoService {
     public CcPaymentInfoResponse getPaymentoInfo(String requestId) {
         Optional<PaymentRequestEntity> paymentInfo = paymentRequestRepository.findByGuidAndRequestEndpoint(requestId, REQUEST_PAYMENTS_VPOS);
 
-        if(!paymentInfo.isPresent()) {
+        if (!paymentInfo.isPresent()) {
             log.error("No CreditCard request entity has been found for requestId: " + requestId);
             throw new CcHttpException(HttpStatus.NOT_FOUND, "RequestId non trovato");
         }
@@ -39,17 +47,32 @@ public class CcPaymentInfoService {
 
     private CcPaymentInfoResponse createInfoPaymentResponse(PaymentRequestEntity paymentInfo) {
         CcPaymentInfoResponse response = new CcPaymentInfoResponse();
+        String requestId = paymentInfo.getGuid();
         response.setStatus(paymentInfo.getStatus());
-        response.setRequestId(paymentInfo.getGuid());
+        response.setRequestId(requestId);
 
-        if(AUTHORIZED.equals(paymentInfo.getStatus())) {
+        if (AUTHORIZED.equals(paymentInfo.getStatus())) {
             ClientConfig clientConfig = clientsConfig.getByKey(paymentInfo.getClientId());
             response.setClientReturnUrl(clientConfig.getVpos().getClientReturnUrl());
         } else {
+            if (METHOD.equals(paymentInfo.getResponseType())) {
+                String threeDsMethodData = generateBase643DsMethodData(paymentInfo.getIdTransaction(), requestId);
+                response.setThreeDsMethodData(threeDsMethodData);
+            }
             response.setResponseType(paymentInfo.getResponseType());
             response.setVposUrl(paymentInfo.getAuthorizationUrl());
         }
 
         return response;
+    }
+
+    private String generateBase643DsMethodData(String idTransaction, String requestId) {
+        String notifyUrl = String.format(methodNotifyUrl, requestId);
+
+        ThreeDsMethodData threeDsMethodData = new ThreeDsMethodData();
+        threeDsMethodData.setTransactionId(idTransaction);
+        threeDsMethodData.setNotificationUrl(notifyUrl);
+
+        return Base64Utils.encodeToString(new Gson().toJson(threeDsMethodData).getBytes());
     }
 }
--- src/test/java/it/pagopa/pm/gateway/service/CcPaymentInfoServiceTest.java
+++ src/test/java/it/pagopa/pm/gateway/service/CcPaymentInfoServiceTest.java
@@ -1,9 +1,13 @@
 package it.pagopa.pm.gateway.service;
 
+import it.pagopa.pm.gateway.beans.ValidBeans;
+import it.pagopa.pm.gateway.dto.ClientConfig;
 import it.pagopa.pm.gateway.dto.vpos.CcHttpException;
 import it.pagopa.pm.gateway.dto.vpos.CcPaymentInfoResponse;
 import it.pagopa.pm.gateway.entity.PaymentRequestEntity;
 import it.pagopa.pm.gateway.repository.PaymentRequestRepository;
+import it.pagopa.pm.gateway.utils.ClientsConfig;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -11,9 +15,12 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
 
+import static it.pagopa.pm.gateway.dto.enums.PaymentRequestStatusEnum.AUTHORIZED;
+import static it.pagopa.pm.gateway.dto.enums.PaymentRequestStatusEnum.CREATED;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -26,16 +33,47 @@ public class CcPaymentInfoServiceTest {
     @InjectMocks
     private CcPaymentInfoService ccPaymentInfoService;
 
+    @Before
+    public void setUpProperties() {
+        ReflectionTestUtils.setField(ccPaymentInfoService, "methodNotifyUrl", "http://localhost:8080");
+    }
+
     @Mock
     private PaymentRequestRepository paymentRequestRepository;
 
+    @Mock
+    private ClientsConfig clientsConfig;
+
     @Test
-    public void getPaymentInfoSuccessTest() {
+    public void getPaymentInfoAuthorizedSuccessTest() {
         PaymentRequestEntity paymentInfo = new PaymentRequestEntity();
-        paymentInfo.setStatus("status");
+        paymentInfo.setStatus(AUTHORIZED.name());
         paymentInfo.setResponseType("type");
         paymentInfo.setGuid("guid");
         paymentInfo.setAuthorizationUrl("url");
+        paymentInfo.setClientId("ClientId");
+        paymentInfo.setIdTransaction("1234");
+
+        ClientConfig clientConfigToReturn = ValidBeans.createClientsConfigVpos();
+
+        when(paymentRequestRepository.findByGuidAndRequestEndpoint(any(), any()))
+                .thenReturn(Optional.of(paymentInfo));
+        when(clientsConfig.getByKey(any())).thenReturn(clientConfigToReturn);
+
+        CcPaymentInfoResponse response = ccPaymentInfoService.getPaymentoInfo("123");
+        assertNotNull(response.getStatus());
+        assertNotNull(response.getRequestId());
+    }
+
+    @Test
+    public void getPaymentInfoNoAuthorizedSuccessTest() {
+        PaymentRequestEntity paymentInfo = new PaymentRequestEntity();
+        paymentInfo.setStatus(CREATED.name());
+        paymentInfo.setResponseType("type");
+        paymentInfo.setGuid("guid");
+        paymentInfo.setAuthorizationUrl("url");
+        paymentInfo.setResponseType("METHOD");
+        paymentInfo.setIdTransaction("12345");
 
         when(paymentRequestRepository.findByGuidAndRequestEndpoint(any(), any()))
                 .thenReturn(Optional.of(paymentInfo));

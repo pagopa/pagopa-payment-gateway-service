1af7f261f73a9689361cf77c40867bfea0601031
Alberto Gelmi <alberto.gelmi@gft.com>
2023-04-19 16:51:38 +0200

    PM-1463: update origin notification vpos

--- src/main/java/it/pagopa/pm/gateway/controller/WebViewVposController.java
+++ src/main/java/it/pagopa/pm/gateway/controller/WebViewVposController.java
@@ -1,7 +1,10 @@
 package it.pagopa.pm.gateway.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,8 +16,13 @@ import static it.pagopa.pm.gateway.constant.ApiPaths.REQUEST_PAYMENTS_VPOS;
 @RequestMapping(REQUEST_PAYMENTS_VPOS)
 public class WebViewVposController {
 
+    @Autowired
+    private Environment environment;
+
     @PostMapping(METHOD_NOTIFICATIONS)
-    public String methodNotifications() {
+    public String methodNotifications(Model model) {
+        String origin = environment.getProperty("vpos.method.origin");
+        model.addAttribute("origin", origin);
         return "methodNotification";
     }
 

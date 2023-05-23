704e98a70865a5ceb928e5026c464845aaec7c77
Alessandro Dossi <alessandro.dossi@external.nexigroup.com>
2023-05-23 17:33:32 +0200

    PM-1536 - commented @Async annotations on VPos and XPay asynchronous services methods except for logic entry points

--- src/main/java/it/pagopa/pm/gateway/service/async/CcResumeStep2AsyncService.java
+++ src/main/java/it/pagopa/pm/gateway/service/async/CcResumeStep2AsyncService.java
@@ -16,7 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -49,7 +48,7 @@ public class CcResumeStep2AsyncService {
         this.ecommercePatchUtils = ecommercePatchUtils;
     }
 
-    @Async
+    //@Async
     public void executeStep2(Map<String, String> params, PaymentRequestEntity entity, StepZeroRequest request) {
         try {
             String requestId = entity.getGuid();
